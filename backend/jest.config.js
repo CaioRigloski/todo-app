81701ad036405b5cba28faec9600de07a8a0771b
Caio Rigloski <caio.henrique.rigloski@hotmail.com>
2025-09-22 10:26:18 -0300

    feat: backend unit test

---
+++ backend/jest.config.js
@@ -0,0 +1,15 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: 'src',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/../',
+  }),
+};
---
+++ backend/src/__tests__/users.service.spec.ts
@@ -0,0 +1,65 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from 'src/users/users.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+describe('UsersService', () => {
+  let service: UsersService;
+  let prisma: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              create: jest.fn().mockResolvedValue({
+                id: 1,
+                email: 'test@example.com',
+                password: 'hashedPassword',
+                name: 'Test User',
+              }),
+              findUnique: jest.fn().mockImplementation(({ where }) => {
+                if (where.email) {
+                  return Promise.resolve({
+                    id: 1,
+                    email: where.email,
+                    password: 'hashedPassword',
+                    name: 'Test User',
+                  });
+                }
+                if (where.id) {
+                  return Promise.resolve({
+                    id: where.id,
+                    email: 'test@example.com',
+                    password: 'hashedPassword',
+                    name: 'Test User',
+                  });
+                }
+                return null;
+              }),
+            },
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should create a user', async () => {
+    const data = { email: 'test@example.com', password: 'hashedPassword', name: 'Test User' };
+    const result = await service.create(data);
+
+    expect(result).toEqual({
+      id: 1,
+      email: 'test@example.com',
+      password: 'hashedPassword',
+      name: 'Test User',
+    });
+
+    expect(prisma.user.create).toHaveBeenCalledWith({ data });
+  });
+});
